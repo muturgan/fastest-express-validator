b5eb0c06ced0954bf16503fcfca4f40288e758dc
Andrey Sakharov <sakharov.a@n-t.io>
2021-12-30 01:27:01 +0300

    publish only if a version was changed

---
+++ publish.helper.js
@@ -0,0 +1,13 @@
+const { execSync } = require('child_process');
+const packageJson = require('./package.json');
+
+// "[ '1.0.0', '1.0.2', '1.0.3', '1.0.4', '1.1.0', '1.2.0' ]"
+const publishedVersionsStr = execSync(`npm view ${packageJson.name} versions`)
+  .toString('utf8')
+  .trim();
+
+const isPublished = publishedVersionsStr.includes(`'${packageJson.version}'`);
+
+if (isPublished !== true) {
+  execSync(`npm publish`);
+}
