3bba75a479b46bb0eb6693d6311da82e9c28cc83
Andrey Sakharov <sakharov.a@n-t.io>
2021-12-30 01:40:29 +0300

    just ci experiments

--- publish.helper.js
+++ publish.helper.js
@@ -8,6 +8,6 @@ const publishedVersionsStr = execSync(`npm view ${packageJson.name} versions`)
 
 const isPublished = publishedVersionsStr.includes(`'${packageJson.version}'`);
 
-if (isPublished !== true) {
-  execSync(`npm publish`);
+if (isPublished === true) {
+  throw new Error('not need to publish');
 }
