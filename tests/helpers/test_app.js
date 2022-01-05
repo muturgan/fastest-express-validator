4e97ba2e0e58bc21ee816a45c0b7c9c666e32a64
Andrey Sakharov <sakharov.a@n-t.io>
2022-01-05 23:00:42 +0300

    DefaultRequestValidator tests were added

--- tests/helpers/test_app.js
+++ tests/helpers/test_app.js
@@ -2,7 +2,7 @@ exports.appFactory = (schema) =>
 {
   const express = require('express');
   const app = express();
-  const { RequestValidator } = require('../../dist');
+  const { RequestValidator, DefaultRequestValidator } = require('../../dist');
 
   app.use(express.json());
 
@@ -30,6 +30,30 @@ exports.appFactory = (schema) =>
   app.post('/body', bodyValidationMiddleware, (req, res) => {
     res.sendStatus(200);
   });
+
+
+
+  const defaultQueryValidationMiddleware = DefaultRequestValidator({
+    query: schema,
+  });
+  app.get('/default/query', defaultQueryValidationMiddleware, (req, res) => {
+    res.sendStatus(200);
+  });
+
+  const defaultParamsValidationMiddleware = DefaultRequestValidator({
+    params: schema,
+  });
+  app.get('/default/params/:name', defaultParamsValidationMiddleware, (req, res) => {
+    res.sendStatus(200);
+  });
+
+  const defaultBodyValidationMiddleware = DefaultRequestValidator({
+    body: schema,
+  });
+  app.post('/default/body', defaultBodyValidationMiddleware, (req, res) => {
+    res.sendStatus(200);
+  });
+
 
 
   app.use((err, req, res, next) => {
--- tests/index.test.js
+++ tests/index.test.js
@@ -12,104 +12,238 @@ const server = app.listen(TEST_PORT, async () =>
 {
   describe('fastest-express-validator', () =>
   {
-    describe('QUERY', () =>
+    describe('RequestValidator', () =>
     {
-      it('missing a required query param', async () => {
-        const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/query`);
-        const parsedBody = await body.json();
+      describe('QUERY', () =>
+      {
+        it('missing a required query param', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/query`);
+          const parsedBody = await body.json();
 
-        assert.strictEqual(statusCode, 500, 'request with a missed required query param is not rejected');
-        assert.strictEqual('query' in parsedBody, true, 'response body does not contains a "query" field');
-        assert.strictEqual(typeof parsedBody?.query?.[0]?.message, 'string', 'response body does not contains an error description');
+          assert.strictEqual(statusCode, 500, 'request with a missed required query param is not rejected');
+          assert.strictEqual('query' in parsedBody, true, 'response body does not contains a "query" field');
+          assert.strictEqual(typeof parsedBody?.query?.[0]?.message, 'string', 'response body does not contains an error description');
+        });
+
+        it('invalid query', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/query?name=xx`);
+          const parsedBody = await body.json();
+
+          assert.strictEqual(statusCode, 500, 'request with an invalid query field is not rejected');
+          assert.strictEqual('query' in parsedBody, true, 'response body does not contains a "query" field');
+          assert.strictEqual(typeof parsedBody?.query?.[0]?.message, 'string', 'response body does not contains an error description');
+        });
+
+        it('correct query', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/query?name=xxx`);
+          for await (const _ of body) {
+            // force consumption of body for a memory leak preventing
+          }
+
+          assert.strictEqual(statusCode, 200, 'request with a correct query field is rejected');
+        });
       });
 
-      it('invalid query', async () => {
-        const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/query?name=xx`);
-        const parsedBody = await body.json();
+      describe('PARAMS', () =>
+      {
+        it('invalid params', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/params/xx`);
+          const parsedBody = await body.json();
 
-        assert.strictEqual(statusCode, 500, 'request with an invalid query field is not rejected');
-        assert.strictEqual('query' in parsedBody, true, 'response body does not contains a "query" field');
-        assert.strictEqual(typeof parsedBody?.query?.[0]?.message, 'string', 'response body does not contains an error description');
+          assert.strictEqual(statusCode, 500, 'request with an invalid url pattern is not rejected');
+          assert.strictEqual('params' in parsedBody, true, 'response body does not contains a "params" field');
+          assert.strictEqual(typeof parsedBody?.params?.[0]?.message, 'string', 'response body does not contains an error description');
+        });
+
+        it('correct params', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/params/xxx`);
+          for await (const _ of body) {
+            // force consumption of body for a memory leak preventing
+          }
+
+          assert.strictEqual(statusCode, 200, 'request with a correct params field is rejected');
+        });
       });
 
-      it('correct query', async () => {
-        const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/query?name=xxx`);
-        for await (const _ of body) {
-          // force consumption of body for a memory leak preventing
-        }
+      describe('BODY', () =>
+      {
+        it('missing a required body field', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/body`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({}),
+          });
+          const parsedBody = await body.json();
 
-        assert.strictEqual(statusCode, 200, 'request with a correct query field is rejected');
+          assert.strictEqual(statusCode, 500, 'request with a missed required body field is not rejected');
+          assert.strictEqual('body' in parsedBody, true, 'response body does not contains a "body" field');
+          assert.strictEqual(typeof parsedBody?.body?.[0]?.message, 'string', 'response body does not contains an error description');
+        });
+
+        it('incomplete body', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/body`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name: 5}),
+          });
+          const parsedBody = await body.json();
+
+          assert.strictEqual(statusCode, 500, 'request with an incomplete body is not rejected');
+          assert.strictEqual('body' in parsedBody, true, 'response body does not contains a "body" field');
+          assert.strictEqual(typeof parsedBody?.body?.[0]?.message, 'string', 'response body does not contains an error description');
+        });
+
+        it('broken body', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/body`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: '{"name: 5}',
+          });
+          const parsedBody = await body.json();
+
+          assert.strictEqual(statusCode, 500, 'runtime error (broken json parsing) is not catched');
+          assert.strictEqual(parsedBody?.type, 'entity.parse.failed', 'response body does not contains an error description');
+        });
+
+        it('correct body', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/body`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name: 'xxx'}),
+          });
+          for await (const _ of body) {
+            // force consumption of body for a memory leak preventing
+          }
+
+          assert.strictEqual(statusCode, 200, 'request with a correct body is rejected');
+        });
       });
     });
 
-    describe('PARAMS', () =>
-    {
-      it('invalid params', async () => {
-        const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/params/xx`);
-        const parsedBody = await body.json();
 
-        assert.strictEqual(statusCode, 500, 'request with a invalid url pattern is not rejected');
-        assert.strictEqual('params' in parsedBody, true, 'response body does not contains a "params" field');
-        assert.strictEqual(typeof parsedBody?.params?.[0]?.message, 'string', 'response body does not contains an error description');
+    describe('DefaultRequestValidator', () =>
+    {
+      describe('QUERY (default)', () =>
+      {
+        it('missing a required query param', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/default/query`);
+          const parsedBody = await body.json();
+
+          assert.strictEqual(statusCode, 422, '(default) request with a missed required query param is not rejected');
+          assert.strictEqual('query' in parsedBody, true, '(default) response body does not contains a "query" field');
+          assert.strictEqual(typeof parsedBody?.query?.[0]?.message, 'string', '(default) response body does not contains an error description');
+        });
+
+        it('invalid query (default)', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/default/query?name=xx`);
+          const parsedBody = await body.json();
+
+          assert.strictEqual(statusCode, 422, '(default) request with an invalid query field is not rejected');
+          assert.strictEqual('query' in parsedBody, true, '(default) response body does not contains a "query" field');
+          assert.strictEqual(typeof parsedBody?.query?.[0]?.message, 'string', '(default) response body does not contains an error description');
+        });
+
+        it('correct query (default)', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/default/query?name=xxx`);
+          for await (const _ of body) {
+            // force consumption of body for a memory leak preventing
+          }
+
+          assert.strictEqual(statusCode, 200, '(default) request with a correct query field is rejected');
+        });
       });
 
-      it('correct params', async () => {
-        const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/params/xxx`);
-        for await (const _ of body) {
-          // force consumption of body for a memory leak preventing
-        }
+      describe('PARAMS (default)', () =>
+      {
+        it('invalid params (default)', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/default/params/xx`);
+          const parsedBody = await body.text();
 
-        assert.strictEqual(statusCode, 200, 'request with a correct params field is rejected');
+          assert.strictEqual(statusCode, 404, '(default) request with an invalid url pattern is not rejected');
+          assert.strictEqual(parsedBody, 'Not Found', '(default) request with an invalid url pattern is not rejected');
+        });
+
+        it('correct params (default)', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/default/params/xxx`);
+          for await (const _ of body) {
+            // force consumption of body for a memory leak preventing
+          }
+
+          assert.strictEqual(statusCode, 200, '(default) request with a correct params field is rejected');
+        });
+      });
+
+      describe('BODY (default)', () =>
+      {
+        it('missing a required body field (default)', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/default/body`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({}),
+          });
+          const parsedBody = await body.json();
+
+          assert.strictEqual(statusCode, 422, '(default) request with a missed required body field is not rejected');
+          assert.strictEqual('body' in parsedBody, true, '(default) response body does not contains a "body" field');
+          assert.strictEqual(typeof parsedBody?.body?.[0]?.message, 'string', '(default) response body does not contains an error description');
+        });
+
+        it('incomplete body (default)', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/default/body`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name: 5}),
+          });
+          const parsedBody = await body.json();
+
+          assert.strictEqual(statusCode, 422, '(default) request with an incomplete body is not rejected');
+          assert.strictEqual('body' in parsedBody, true, '(default) response body does not contains a "body" field');
+          assert.strictEqual(typeof parsedBody?.body?.[0]?.message, 'string', '(default) response body does not contains an error description');
+        });
+
+        it('broken body (default)', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/default/body`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: '{"name: 5}',
+          });
+          const parsedBody = await body.json();
+
+          assert.strictEqual(statusCode, 500, '(default) runtime error (broken json parsing) is not catched');
+          assert.strictEqual(parsedBody?.type, 'entity.parse.failed', '(default) response body does not contains an error description');
+        });
+
+        it('correct body (default)', async () => {
+          const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/body`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name: 'xxx'}),
+          });
+          for await (const _ of body) {
+            // force consumption of body for a memory leak preventing
+          }
+
+          assert.strictEqual(statusCode, 200, '(default) request with a correct body is rejected');
+        });
       });
     });
 
-    describe('BODY', () =>
-    {
-      it('missing a required body field', async () => {
-        const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/body`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({}),
-        });
-        const parsedBody = await body.json();
-
-        assert.strictEqual(statusCode, 500, 'request with a missed required body field is not rejected');
-        assert.strictEqual('body' in parsedBody, true, 'response body does not contains a "body" field');
-        assert.strictEqual(typeof parsedBody?.body?.[0]?.message, 'string', 'response body does not contains an error description');
-      });
-
-      it('invalid body', async () => {
-        const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/body`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({name: 5}),
-        });
-        const parsedBody = await body.json();
-
-        assert.strictEqual(statusCode, 500, 'request with a invalid body is not rejected');
-        assert.strictEqual('body' in parsedBody, true, 'response body does not contains a "body" field');
-        assert.strictEqual(typeof parsedBody?.body?.[0]?.message, 'string', 'response body does not contains an error description');
-      });
-
-      it('correct body', async () => {
-        const {statusCode, body} = await fetch(`http://localhost:${TEST_PORT}/body`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({name: 'xxx'}),
-        });
-        for await (const _ of body) {
-          // force consumption of body for a memory leak preventing
-        }
-
-        assert.strictEqual(statusCode, 200, 'request with a correct body is rejected');
-      });
-    });
 
     after(() => {
       return new Promise((resolve, reject) => {
