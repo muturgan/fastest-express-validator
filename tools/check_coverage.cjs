74304ae150e4052303a8ef931c455c6bb9c0f122
Andrey Sakharov <sakharov.a@n-t.io>
2022-01-05 05:18:02 +0300

    coverage checking was added

---
+++ tools/check_coverage.cjs
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const MIN_COVERAGE_PERCENT = Number(process.env.MIN_COVERAGE_PERCENT) || 80;
+const COVERAGE_INFO_PATH = path.join(process.cwd(), 'coverage', 'coverage-summary.json');
+
+const coverageInfo = require(COVERAGE_INFO_PATH);
+
+if (
+        coverageInfo.total.statements.pct < MIN_COVERAGE_PERCENT
+    ||  coverageInfo.total.functions.pct  < MIN_COVERAGE_PERCENT
+) {
+    throw new Error('code coverage is low');
+}
+
+console.info('code coverage is enough');
--- tools/publish.helper.cjs
+++ tools/publish.helper.cjs
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process');
-const packageJson = require('./package.json');
+const packageJson = require('../package.json');
 
 // "[ '1.0.0', '1.0.2', '1.0.3', '1.0.4', '1.1.0', '1.2.0' ]"
 const publishedVersionsStr = execSync(`npm view ${packageJson.name} versions`)
