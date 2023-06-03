07a1ea83c8373d8e271ace3e4c43c45008a14196
Andrey Sakharov <sakharov.a@n-t.io>
2023-06-04 00:51:47 +0300

    calculate coverage color with native node.js tools

--- tools/get_coverage_color.cjs
+++ tools/get_coverage_color.cjs
@@ -1,15 +1,22 @@
+const fs = require('node:fs');
+const { EOL } = require('node:os');
 const path = require('node:path');
 
-const COVERAGE_INFO_PATH = path.join(process.cwd(), 'coverage', 'coverage-summary.json');
+const COVERAGE_INFO_PATH = path.join(process.cwd(), 'coverage.txt');
 
-const coverageInfo = require(COVERAGE_INFO_PATH);
-const coverage = coverageInfo.total.branches.pct;
+const coverageInfo = fs.readFileSync(COVERAGE_INFO_PATH).toString();
+
+const lines = coverageInfo.split(EOL);
+
+const libCoverageInfo = lines.find((l) => l.includes('index.cjs'));
+const coverageValues = libCoverageInfo.split('|').map((v) => Number(v.trim()));
+const [, , branchesCoverage] = coverageValues;
 
 let coverageColor = 'green';
-if (coverage <= 80) {
+if (branchesCoverage <= 80) {
     coverageColor = 'orange';
 }
-if (coverage <= 60) {
+if (branchesCoverage <= 60) {
     coverageColor = 'red';
 }
 
