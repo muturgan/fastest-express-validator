8725d08884b1a7b077fd4dff55dafdf5ed180561
Andrey Sakharov <sakharov.a@n-t.io>
2023-06-04 00:20:18 +0300

    using native node code coverage tools

--- tools/check_coverage.cjs
+++ tools/check_coverage.cjs
@@ -1,16 +1,24 @@
+const fs = require('node:fs');
+const { EOL } = require('node:os');
 const path = require('node:path');
 
 const MIN_COVERAGE_PERCENT = Number(process.env.MIN_COVERAGE_PERCENT) || 80;
-const COVERAGE_INFO_PATH = path.join(process.cwd(), 'coverage', 'coverage-summary.json');
+const COVERAGE_INFO_PATH = path.join(process.cwd(), 'coverage.txt');
 
-const coverageInfo = require(COVERAGE_INFO_PATH);
+const coverageInfo = fs.readFileSync(COVERAGE_INFO_PATH).toString();
+
+const lines = coverageInfo.split(EOL);
+
+const libCoverageInfo = lines.find((l) => l.includes('index.cjs'));
+const coverageValues = libCoverageInfo.split('|').map((v) => Number(v.trim()));
+const [, linesCoverage, branchesCoverage, funcsCoverage] = coverageValues;
 
 if (
-        coverageInfo.total.statements.pct < MIN_COVERAGE_PERCENT
-    ||  coverageInfo.total.functions.pct  < MIN_COVERAGE_PERCENT
-    ||  coverageInfo.total.branches.pct   < MIN_COVERAGE_PERCENT
+        linesCoverage < MIN_COVERAGE_PERCENT
+    ||  branchesCoverage  < MIN_COVERAGE_PERCENT
+    ||  funcsCoverage   < MIN_COVERAGE_PERCENT
 ) {
-    throw new Error('code coverage is low');
+    throw new Error('code coverage is too low');
 }
 
-console.info('code coverage is enough');
+console.info(branchesCoverage + '%');
